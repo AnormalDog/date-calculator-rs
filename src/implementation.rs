0cc3c8fd1711b014f32d50a39ed75d253eb32985
AnormalDog <150300262+AnormalDog@users.noreply.github.com>
2025-06-06 18:06:18 +0100

    some implementation functions added, decided the basic layout

---
+++ src/implementation.rs
@@ -0,0 +1,72 @@
+//! Author: AnormalDog (https://github.com/AnormalDog)
+//! Copyright (c) 2025 AnormalDog
+//! Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//!
+//! Internal implementation of the library
+
+use crate::Months;
+
+/// Returns true or false if the year is leap or not
+pub fn is_year_leap(year: i32) -> bool {
+  if year % 4 != 0 {
+    return false;
+  }
+  if year % 100 != 0 {
+    return true;
+  }
+  if year % 400 == 0 { true } else { false }
+}
+
+/// Returns if a year is valid or not
+/// For now, the function quite simple, not sure if I will need to do more
+///   checks
+fn is_year_valid(year: i32) -> bool {
+  if year == 0 {
+    return false;
+  };
+  return true;
+}
+
+/// Returns the number of days a months has.
+/// If the month is february, returns 0, which means that need to consideer the year
+fn get_number_of_days(month: Months) -> u32 {
+  match month {
+    Months::Jan
+    | Months::Mar
+    | Months::May
+    | Months::Jul
+    | Months::Aug
+    | Months::Oct
+    | Months::Dec => 31,
+    Months::Feb => 0,
+    _ => 30,
+  }
+}
+
+/// Returs the max number of days a months can hold
+fn max_num_days(date: &crate::Date) -> u32 {
+  match date.month {
+    Months::Feb => {
+      if is_year_leap(date.year) == true {
+        29
+      } else {
+        28
+      }
+    }
+    _ => get_number_of_days(date.month),
+  }
+}
+
+/// Check if a object of Struct Date is correct and valid
+pub fn is_date_correct(date: &crate::Date) -> bool {
+  // check year
+  if is_year_valid(date.year) == false {
+    return false;
+  }
+  // check days
+  let max_num_days: u32 = max_num_days(date);
+  if date.day == 0 || date.day > max_num_days {
+    return false;
+  }
+  return true;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Author: AnormalDog (https://github.com/AnormalDog)
+//! Copyright (c) 2025 AnormalDog
+//! Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//!
+//! Internal implementation of the library
+
+mod implementation;
+
+struct Date {
+  day: u32,
+  month: Months,
+  year: i32,
+}
+#[derive(Clone, Copy)]
+enum Months {
+  Jan,
+  Feb,
+  Mar,
+  Apr,
+  May,
+  Jun,
+  Jul,
+  Aug,
+  Sep,
+  Oct,
+  Nov,
+  Dec,
+}
+
+impl Date {
+  pub fn new(day: u32, month: Months, year: i32) -> Self {}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use crate::implementation;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+  #[test]
+  fn test_is_leap() {
+    let x =  implementation::is_year_leap(1996);
+    assert_eq!(x, true);
+  }
 }
