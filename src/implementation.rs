700e4cc0b1effe8649cd6608cb3bc94e417155eb
AnormalDog <150300262+AnormalDog@users.noreply.github.com>
2025-06-10 23:47:23 +0100

    do basic structure

--- src/implementation.rs
+++ src/implementation.rs
@@ -4,3 +4,18 @@
 //!
 //! Internal implementation of the library
 
+use crate::{Date, DateError};
+
+fn is_year_leap(year: i32) -> bool {
+  if year % 4 != 0 {
+    false
+  } else if year % 100 != 0 {
+    true
+  } else {
+    year % 400 == 0
+  }
+}
+
+fn days_per_month(year: i32, part: u32) -> u32 {}
+
+pub fn validate_raw_input(year: i32, month: u32, day: u32) -> Result<(), DateError> {}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,40 @@
 //! Licensed under the MIT License. See LICENSE file in the project root for full license information.
 //!
 //! Library
+
+mod implementation;
+
+use std::{fmt, ops::Rem};
+
+#[derive(Clone, Copy, Debug)]
+pub enum DateError {
+  InvalidRawInput,
+  OutOfLimits,
+}
+
+impl fmt::Display for DateError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match *self {
+      DateError::InvalidRawInput => f.write_str("Invalid input introducing raw values"),
+      DateError::OutOfLimits => {
+        f.write_str("the date is outside of the limits (-9999 - 9999) years")
+      }
+    }
+  }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Date {
+  year: i32,
+  part: u32,
+}
+
+impl fmt::Display for Date {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    f.write_str("data")
+  }
+}
+
+impl Date {
+  pub fn new(year: i32, month: u32, day: u32) -> Result<Self, DateError> {}
+}
