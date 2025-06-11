10bf16e0fb0ff6adf8bf3b16f8821d004da856de
AnormalDog <150300262+AnormalDog@users.noreply.github.com>
2025-06-11 16:24:05 +0100

    add basic fn and methods, and fn add days

--- src/implementation.rs
+++ src/implementation.rs
@@ -6,6 +6,7 @@
 
 use crate::{Date, DateError};
 
+/// Returns true is the year is leap
 fn is_year_leap(year: i32) -> bool {
   if year % 4 != 0 {
     false
@@ -16,6 +17,7 @@ fn is_year_leap(year: i32) -> bool {
   }
 }
 
+/// Returns the max number a month can hold
 fn days_per_month(year: i32, month: u32) -> u32 {
   assert!(month > 0 && month <= 12);
   match month {
@@ -26,15 +28,74 @@ fn days_per_month(year: i32, month: u32) -> u32 {
   }
 }
 
-pub fn validate_raw_input(year: i32, month: u32, day: u32) -> Result<(), DateError> {
-  if year == 0 ||
-    month == 0 || month > 12 ||
-    day == 0 || day > days_per_month(year, month) {
-      Err(DateError::InvalidRawInput)
-    }
-    else {
-      Ok(())
-    }
+/// Returns the number of day a year has
+fn days_per_year(year: i32) -> u32 {
+  if is_year_leap(year) { 366 } else { 365 }
 }
 
-fn get
+/// validates if the raw data is correct
+pub fn validate_raw(year: i32, month: u32, day: u32) -> Result<(), DateError> {
+  if year < -9999 || year > 9999 {
+    Err(DateError::OutOfLimits)
+  } else if (year == 0)
+    || (month == 0)
+    || (month > 12)
+    || (day == 0)
+    || (day > days_per_month(normalize_year(year), month))
+  {
+    Err(DateError::InvalidNewInput)
+  } else {
+    Ok(())
+  }
+}
+
+/// Validate the current state of the instance
+pub fn validate(date: &Date) -> Result<(), DateError> {
+  assert!(date.index < days_per_year(date.year));
+  if date.year > 9999 || date.year < -9998 {
+    Err(DateError::OutOfLimits)
+  } else {
+    Ok(())
+  }
+}
+
+/// Convert from Y/M/D -> Y/D
+pub fn year_index(year: i32, month: u32, day: u32) -> u32 {
+  (1..month).map(|x| days_per_month(year, x)).sum::<u32>() + day
+}
+
+/// Move all BC(negative) years one number to right, making the 1BC the 0, the 2BC the -1...
+pub fn normalize_year(year: i32) -> i32 {
+  assert_ne!(year, 0);
+  assert!(year >= -9999 && year <= 9999);
+  if year < 0 { year + 1 } else { year }
+}
+
+/// Add n days
+pub fn add_n_days(date: &mut Date, n: u32) {
+  date.index += n;
+  while date.index > days_per_year(date.year) {
+    date.index -= days_per_year(date.year);
+    date.year += 1;
+  }
+}
+
+#[cfg(test)]
+mod implementation_test {
+  use crate::{Date, DateError};
+  use crate::implementation::{add_n_days, validate_raw};
+  #[test]
+  fn validate_raw_test() {
+    validate_raw(10000, 5, 12).expect_err("correct error");
+    validate_raw(2000, 2, 29).expect("correct no error");
+    validate_raw(1, 2, 29).expect_err("correct error");
+  }
+
+  #[test]
+  fn add_days_test() {
+    let mut y = Date::new(2000, 2, 29).expect("error creating the obj test");
+    add_n_days(&mut y, 1000);
+    let z = Date::new(2002, 11, 25).expect("error creating the obj test");
+    assert_eq!(y, z);
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,19 @@
 
 mod implementation;
 
-use std::{fmt, ops::Rem};
+use crate::implementation::{add_n_days, normalize_year, validate, validate_raw, year_index};
+use std::fmt;
 
 #[derive(Clone, Copy, Debug)]
 pub enum DateError {
-  InvalidRawInput,
+  InvalidNewInput,
   OutOfLimits,
 }
 
 impl fmt::Display for DateError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match *self {
-      DateError::InvalidRawInput => f.write_str("Invalid input introducing raw values"),
+      DateError::InvalidNewInput => f.write_str("Invalid input introducing values to NEW"),
       DateError::OutOfLimits => {
         f.write_str("the date is outside of the limits (-9999 - 9999) years")
       }
@@ -25,10 +26,10 @@ impl fmt::Display for DateError {
   }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Date {
   year: i32,
-  part: u32,
+  index: u32,
 }
 
 impl fmt::Display for Date {
@@ -38,5 +39,39 @@ impl fmt::Display for Date {
 }
 
 impl Date {
-  pub fn new(year: i32, month: u32, day: u32) -> Result<Self, DateError> {}
+  /// Create a new instance of Date
+  pub fn new(year: i32, month: u32, day: u32) -> Result<Date, DateError> {
+    validate_raw(year, month, day)?;
+    let year = normalize_year(year);
+    let x = Date {
+      year: year,
+      index: year_index(year, month, day),
+    };
+    Ok(x)
+  }
+
+  /// Add days to an instance, then checks if still valid
+  pub fn add_days(&mut self, days: u32) -> Result<&Self, DateError> {
+    add_n_days(self, days);
+    validate(self)?;
+    Ok(self)
+  }
+}
+
+#[cfg(test)]
+mod lib_test {
+  use crate::Date;
+  #[test]
+  fn new_test() {
+    let _w = Date::new(2000, 2, 29).expect("error in w");
+    let _x = Date::new(2001, 2, 29).expect_err("expected error in x");
+    let _y = Date::new(-1, 2, 28).expect("error in y");
+    let _z = Date::new(0, 50, 15).expect_err("error expected in z");
+  }
+
+  #[test]
+  fn add_days_test() {
+    let mut y = Date::new(9999, 12, 31).expect("error creating the obj test");
+    y.add_days(1).expect_err("no error?");
+  }
 }
