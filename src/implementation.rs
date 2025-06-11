d4bc5f23f1f099285463a268bda33fe8792d6461
AnormalDog <150300262+AnormalDog@users.noreply.github.com>
2025-06-11 09:45:13 +0100

    do fn validate_raw_input

--- src/implementation.rs
+++ src/implementation.rs
@@ -16,6 +16,25 @@ fn is_year_leap(year: i32) -> bool {
   }
 }
 
-fn days_per_month(year: i32, part: u32) -> u32 {}
+fn days_per_month(year: i32, month: u32) -> u32 {
+  assert!(month > 0 && month <= 12);
+  match month {
+    1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+    2 if is_year_leap(year) => 29,
+    2 if !is_year_leap(year) => 28,
+    _ => 30,
+  }
+}
 
-pub fn validate_raw_input(year: i32, month: u32, day: u32) -> Result<(), DateError> {}
+pub fn validate_raw_input(year: i32, month: u32, day: u32) -> Result<(), DateError> {
+  if year == 0 ||
+    month == 0 || month > 12 ||
+    day == 0 || day > days_per_month(year, month) {
+      Err(DateError::InvalidRawInput)
+    }
+    else {
+      Ok(())
+    }
+}
+
+fn get
